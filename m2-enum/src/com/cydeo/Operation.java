efcf5cfab78583e9f321b09385aeb354ce8630e4
Hilal2013 <hilalozkul@gmail.com>
2023-01-23 18:43:06 -0500

    updated

--- m2-enum/src/com/cydeo/Operation.java
+++ m2-enum/src/com/cydeo/Operation.java
@@ -2,7 +2,7 @@ package com.cydeo;
 
 public enum Operation {
     PLUS,MINUS,MULTIPLY,DIVIDE; //each constant is object(new) of Operation
-
+// how can assign value to enum constants/Can I assign?
     Operation() {
         System.out.println("constructor");//we created 4 objects so when we call in method it will print 4 times constructor
         //each new keyword(new object) executes constructor
