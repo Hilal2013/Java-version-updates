78d3b7d5d38a241549e6e06f405a1fe6aea7521d
Hilal2013 <hilalozkul@gmail.com>
2023-01-29 21:40:54 -0500

    added task

---
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,11 @@
+package com.cydeo;
+
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+
+       Predicate pred= (p)->p%2==0 ? true:false;
+
+    }
+}
