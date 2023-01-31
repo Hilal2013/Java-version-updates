01721bbb65c062d9beb51454006b485135053d1f
Hilal2013 <hilalozkul@gmail.com>
2023-01-30 23:37:45 -0500

    added PrintNumberOfCharacters

---
+++ m6-stream/src/main/java/com/cydeo/Tasks/PrintNumberOfCharacters.java
@@ -0,0 +1,17 @@
+package com.cydeo.Tasks;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PrintNumberOfCharacters {
+    public static void main(String[] args) {
+        List<String> words= Arrays.asList("JAVA","APPLE","HONDA","DEVELOPER");
+
+        words.stream()
+                .map(word->word.length())
+                .forEach(System.out::println);
+
+
+
+    }
+}
