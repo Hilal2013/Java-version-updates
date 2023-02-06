47d7973b3cd2ce867c31e49165f618def88e9d7c
Hilal2013 <hilalozkul@gmail.com>
2023-02-06 17:19:18 -0500

    updated

--- m8-new-version-updates/src/main/java/com/cydeo/java9/ModuleDemo.java
+++ m8-new-version-updates/src/main/java/com/cydeo/java9/ModuleDemo.java
@@ -1,4 +1,14 @@
 package com.cydeo.java9;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class ModuleDemo {
+    public static void main(String[] args) {
+        System.out.println(String.class.getModule());
+        System.out.println(List.class.getModule());
+        System.out.println(Scanner.class.getModule());
+       // module java.base->all of them belongs to java.base module
+
+    }
 }
