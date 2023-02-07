7d7d951bf49f496acce8db13ca0ebfc3c968a71d
Hilal2013 <hilalozkul@gmail.com>
2023-02-07 13:51:35 -0500

    updated

--- m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
@@ -1,4 +1,15 @@
 package com.cydeo.java14;
 
-public class User {
+public record User(String name, String address) {
+    // you ca create no arg cons
+    public User{}
+
+    //now you have
+    //ALL ARG CONST
+    //GETTER
+    //TO STRING
+    //EQUALS
+    //HASH CODE
+
+   // but there is no Setter why because it is immutable, final
 }
