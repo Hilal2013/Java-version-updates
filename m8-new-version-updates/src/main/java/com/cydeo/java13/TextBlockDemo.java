39c8ea0acc4eb1c91ab0e5f62ca94d489db4c5ef
Hilal2013 <hilalozkul@gmail.com>
2023-02-07 12:46:52 -0500

    created java14 package

--- m8-new-version-updates/src/main/java/com/cydeo/java13/TextBlockDemo.java
+++ m8-new-version-updates/src/main/java/com/cydeo/java13/TextBlockDemo.java
@@ -2,13 +2,13 @@ package com.cydeo.java13;
 
 public class TextBlockDemo {
     public static void main(String[] args) {
-var address="7925 Jones Branch Drve \n" + "Mclean,VA 22104";
+var address="7925 Jones Branch Drive \n" + "Mclean,VA 22104";
         System.out.println(address);
 //7925 Jones Branch Drve
 //Mclean,VA 22104
         //just open three double quotes//it is coming from java13
         var address2= """
-               7925 Jones Branch Drve 
+               7925 Jones Branch Drive 
                  22104
                 """;
         System.out.println(address2);
