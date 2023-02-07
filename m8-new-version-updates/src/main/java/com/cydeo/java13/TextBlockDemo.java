b0765323b59e036bd8b1693c60e4e58968111161
Hilal2013 <hilalozkul@gmail.com>
2023-02-07 12:22:01 -0500

    created java13 package

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java13/TextBlockDemo.java
@@ -0,0 +1,17 @@
+package com.cydeo.java13;
+
+public class TextBlockDemo {
+    public static void main(String[] args) {
+var address="7925 Jones Branch Drve \n" + "Mclean,VA 22104";
+        System.out.println(address);
+//7925 Jones Branch Drve
+//Mclean,VA 22104
+        //just open three double quotes//it is coming from java13
+        var address2= """
+               7925 Jones Branch Drve 
+                 22104
+                """;
+        System.out.println(address2);
+
+    }
+}
