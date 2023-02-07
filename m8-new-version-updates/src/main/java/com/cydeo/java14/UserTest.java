98068b4bdc82a5f70427cbac665af4a5556558f1
Hilal2013 <hilalozkul@gmail.com>
2023-02-07 13:51:27 -0500

    updated

--- m8-new-version-updates/src/main/java/com/cydeo/java14/UserTest.java
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/UserTest.java
@@ -1,4 +1,13 @@
 package com.cydeo.java14;
 
 public class UserTest {
+    public static void main(String[] args) {
+        //That is the proof
+        User user = new User("Mike","VA");
+     //   System.out.println(name);//compile error//why i cannot call directly because field is final private/
+        //   /need getters
+        System.out.println(user.name());//through original name
+        System.out.println(user.toString());
+
+    }
 }
